a66801e4006d994b109b359790e34240e9f13002
Vladislava Musin <vlada.musin@gmail.com>
2025-05-20 22:57:00 +0300

    implemented Springfox

--- src/main/java/com/movies/config/SecurityConfiguration.java
+++ src/main/java/com/movies/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
         http.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
+                .requestMatchers("/auth/**", "/v2/api-docs")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
